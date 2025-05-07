e7f291a5db4e52e5b871fe912bd27c70e2958c81
minhyuk <martingodfamily@gmail.com>
2025-05-07 20:20:36 +0900

    To-Do List 실습

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const handleIncrease = () => {
+    setCount((prev) => prev + 1);
+  };
+
   const handleDecrease = () => {
     setCount((prev) => prev - 1);
   };
@@ -11,37 +15,35 @@ const Counter = () => {
     setCount(0);
   };
 
-  const handleIncrease = () => {
-    setCount((prev) => prev + 1);
-  };
+  const btnStyle =
+    "px-4 py-2 bg-white border hover:bg-yellow-300 cursor-pointer rounded-2xl"
 
   return (
     <div className="flex flex-col bg-gray-100 p-10">
       <h1 className="text-3xl font-bold mb-4">Counter</h1>
       <p className="text-2xl mb-6">현재 값: {count}</p>
-      <button
-        onClick={handleIncrease}
-        className="w-15 h-10 px-4 py-2 bg-white border hover:bg-yellow-300 cursor-pointer rounded-2xl"
-      >
-        +1
-      </button>
-      <button
-        onClick={handleDecrease}
-        className="w-15 h-10 px-4 py-2 bg-white border hover:bg-yellow-300 cursor-pointer rounded-2xl"
-      >
-        -1
-      </button>
-      <button
-        onClick={handleReact}
-        className="w-15 h-10 px-4 py-2 bg-white border hover:bg-yellow-300 cursor-pointer rounded-2xl"
-      >
-        Reset
-      </button>
-      
+      <div className="flex gap-2">
+        <button
+          onClick={handleIncrease}
+          className={btnStyle}
+          >
+          +1
+        </button>
+        <button
+          onClick={handleDecrease}
+          className={btnStyle}
+          >
+          -1
+        </button>
+        <button
+          onClick={handleReset}
+          className={btnStyle}
+          >
+          Reset
+        </button>
+      </div>
     </div>
   );
-
-
 };
 
 export default Counter;
