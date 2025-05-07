6829c34f53e4115e1c58efc482bd6d874cc99788
minhyuk <martingodfamily@gmail.com>
2025-05-07 19:27:11 +0900

    Counter 실습

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,6 +3,14 @@ import React, { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const handleDecrease = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   const handleIncrease = () => {
     setCount((prev) => prev + 1);
   };
@@ -17,8 +25,23 @@ const Counter = () => {
       >
         +1
       </button>
+      <button
+        onClick={handleDecrease}
+        className="w-15 h-10 px-4 py-2 bg-white border hover:bg-yellow-300 cursor-pointer rounded-2xl"
+      >
+        -1
+      </button>
+      <button
+        onClick={handleReact}
+        className="w-15 h-10 px-4 py-2 bg-white border hover:bg-yellow-300 cursor-pointer rounded-2xl"
+      >
+        Reset
+      </button>
+      
     </div>
   );
+
+
 };
 
 export default Counter;
